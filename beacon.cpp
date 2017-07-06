43c81c2b75d93497f77c44a1607aa197f4335b5c
diealecel <diealecel@gmail.com>
2017-07-06 10:28:37 -0700

    fixed some indentation

--- beacon.cpp
+++ beacon.cpp
@@ -3,24 +3,24 @@
 include "beacon.h"
 
 void beacon_init(int id, double x, double y){
-  beacon_id = id;
-  beaconx = x;
-  beacony = y;
+    beacon_id = id;
+    beaconx = x;
+    beacony = y;
 }
 
 int get_ID(){
-  return beacon_id;
+    return beacon_id;
 }
 
 double get_x(){
-  return beaconx;
+    return beaconx;
 }
 
 double get_y(){
-  return beacony;
+    return beacony;
 }
 
 ostream& operator<<(ostream& out, const Beacon& b){
-	out << "Beacon " << beacon_id << " at (" << beaconx << ", " << beacony << ")" << endl; 
+    out << "Beacon " << beacon_id << " at (" << beaconx << ", " << beacony << ")" << endl; 
     return out;
 }
--- beacon.h
+++ beacon.h
@@ -9,16 +9,16 @@ using namespace std;
 
 class Beacon {
 public:
-   	void beacon_init(int id, double x, double y);
-	int getID() const;
-	double getx() const;
-	double gety() const;
-	friend ostream& operator <<(ostream& out, const Beacon& b);
+    void beacon_init(int id, double x, double y);
+    int getID() const;
+    double getx() const;
+    double gety() const;
+    friend ostream& operator <<(ostream& out, const Beacon& b);
 
 private:
-	int beacon_id;
+    int beacon_id;
     double beaconx;
-  	double beacony;
+    double beacony;
 };
 
 #endif
