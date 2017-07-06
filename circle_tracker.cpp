d90161b9049e18ae06898da1c2c775e01b3e2658
diealecel <diealecel@gmail.com>
2017-07-06 12:27:23 -0700

    dereferencing works now

--- circle_tracker.cpp
+++ circle_tracker.cpp
@@ -13,7 +13,7 @@
 
 //int beacon_ids[NUM_BEACONS] = {}
 
-/*
+
 int main() {
     // Map initialization.
     double map_length = -1;
@@ -44,69 +44,49 @@ int main() {
 
     // Initialize the beacons
 
-    for(int i = 0; i < NUM_BEACONS; i++)
-}
-*/
+    for(int i = 0; i < NUM_BEACONS; i++) {
+        double bx = -1;
 
+        cout << "Enter x-coordinate for beacon " << i << ":" << endl;
+        cin >> bx;
 
-int main() {
-    // Map Initialization
-    double map_length = -1;  
-      cout << "Enter map length" << endl; 
-      cin >> map_length; 
-      while(map_length < 0){
-        cout << "Please enter a positivie map length" << endl; 
-        cin >> map_length; 
-      }
-      double map_width = -1; 
-      cout <<"Enter map width" << endl; 
-      cin >> map_width; 
-      while(map_width < 0){
-        cout << "Please enter a positivie map width" << endl; 
-        cin >> map_width; 
-      }
-      cout << "Map dimensions: (" << map_length << ", " << map_width << ")" << endl; 
+        while(bx < 0 || bx > map_width) {
+            cout << "Beacon " << i << " out of bounds." << endl;
+            cin >> bx;
+        }
 
-  //build map       
+        double by = -1;
 
-  
+        cout << "Enter y-coordinate for beacon " << i << ":" << endl;
+        cin >> by;
 
-  Beacon* beacons[NUM_BEACONS];
-  //Initialize the beacons 
+        while(by < 0 || by > map_length) {
+            cout << "Nope." << endl;
+            cin >> by;
+        }
+        
+        beacons[i] = new Beacon(i, bx, by);
+    }
 
-    for(int i=0; i<NUM_BEACONS; i++){                                                                                                                                                                       
-        double bx = -1;                                                                                                                                                                                        
-      cout << "Enter x coordinate for beacon " << i << ":" << endl; 
-      cin >> bx;  
-      while(bx < 0 || bx > map_width){
-         cout << "Beacon " << i << " out of bounds, please enter valid x coordinate" << endl;
-         cin >> bx;  
-      }
-      double by = -1;                                                                                                                                                                                        
-      cout << "Enter y coordinate for beacon " << i << ":" << endl; 
-      cin >> by;   
-      while(by < 0 || by > map_length){
-          cout << "Beacon " << i << " out of bounds, please enter valid y coordinate" << endl;
-          cin >> by;    
-     }                                                                                                                                                                                 
-      beacons[i] = new Beacon(i, bx, by);
-    }          
+    for(int i = 0; i < NUM_BEACONS; i++) {
+        cout << *beacons[i];
+    }
+    
+    // Wait for user to be ready for tracking.
+    //system("pause")
 
-  for(int i =0; i<NUM_BEACONS; i++){
-    cout << beacons[i]; 
-  }                                                                                                                                                                                           
+    while(true) {
+        // Get beacon numbers and radii from app
 
-  //wait for user to be ready for tracking                                                                                                                                                                
-  //system("pause");
+        int b0, b1, b2;
+        double r0, r1, r2;
 
-  while(true){
-      //get beacon numbers and radii from app                                                                                                                                                                                                                                                                                                                                                                
-      int b0, b1, b2;                                                                                                                                                                                     
-      double r0, r1, r2;                                                                                                                                                                                  
-      //get data                                                                                                                                                                                          
-      //calculateThreeCircleIntersection(beacons[b0].getx(), beacons[b0].gety(), r0, beacons[b1].getx(), beacons[b1].gety(), r1, beacons[b2].getx(), beacons[b2].gety(), r2);                               
-      // graphics                                                                                                                                                                                     
- }                                                                                                                                                                                                                                                                                                                                                                       
-  return 1;
+        //get data
+
+        //calculateThreeCircleIntersection(beacons[b0].getx(), beacons[b0].gety(), r0, beacons[b1].getx(), beacons[b1].gety(), r1, beacons[b2].getx(), beacons[b2].gety(), r2);
+
+        //graphics
+
+    }
 }
 
